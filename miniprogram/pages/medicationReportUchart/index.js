07ae2bb01b7f2526231810e67dee24ea4a37f6da
Yawen Guo <jennyguo@uw.edu>
2022-11-04 16:48:20 -0700

    time period button clickable

--- miniprogram/pages/medicationReportUchart/index.js
+++ miniprogram/pages/medicationReportUchart/index.js
@@ -1,11 +1,24 @@
 // import * as ucharts from '@qiun/ucharts/';
 import uCharts from '../../miniprogram_npm/@qiun/ucharts/index.js';
 
+const app = getApp()
+var uChartsInstance = {};
+
 var uChartsInstance = {};
 Page({
   data: {
     cWidth: 340,
-    cHeight: 220
+    cHeight: 220,
+    pixelRatio: 2,
+    userScoreValue: '',
+    userScoreType: '情绪状况',
+    userScoreColor: '#7B7B7B',
+    oneMonth: true,
+    threeMonth: false,
+    sixMonth: false,
+    timePeriodText: '近一月',
+    timePeirodDate: '02月20日-02月27日',
+    res_index: 0
   },
   onReady() {
     let cWidth = this.data.cWidth;
@@ -17,14 +30,34 @@ Page({
     //模拟从服务器获取数据时的延时
     setTimeout(() => {
       //模拟服务器返回数据，如果数据格式和标准格式不同，需自行按下面的格式拼接
-      let res = {
-        categories: ["01.30-02.05","02.06-02.12","02.13-02.19","02.20-02.27","02.28-03.04","03.05-03.11"],
-        series: [
-          {
-            data: [1,0,2,3,4,4],
-          },
-        ]
-      };
+      // let res = {
+      //   // categories: [],
+      //   categories: ["01.30-02.05","02.06-02.12","02.13-02.19","02.20-02.27","02.28-03.04","03.05-03.11"],
+      //   series: [
+      //     {
+      //       // data: [],
+      //       data: [1,0,2,3,4,4],
+
+      //     },
+      //   ]
+      // };
+
+
+      let res_pre = [
+        // 近1月
+        { categories: ["01.30-02.05","02.06-02.12","02.13-02.19","02.20-02.27","02.28-03.04","03.05-03.11"],
+          series: [{data: [1,0,2,3,4,4]}]},
+
+        // 近3月
+        { categories: ["01.30-02.05","02.06-02.12","02.13-02.19","02.20-02.27","02.28-03.04","03.05-03.11"],
+          series: [{data: [6,7,9,12,4,3]}]},
+        
+        // 近6月
+        { categories: ["01.30-02.05","02.06-02.12","02.13-02.19","02.20-02.27","02.28-03.04","03.05-03.11"],
+          series: [{data: [8,0,12,3,14,4]}]},
+      ];
+
+      var res = res_pre[0]
       this.drawCharts('jkyWEuYZpJWLcfbnKkmySDRjQLEpHsIG', res);
     }, 500);
   },
@@ -68,6 +101,7 @@ Page({
         }
       });
   },
+
   touchstart(e){
     uChartsInstance[e.target.id].scrollStart(e);
   },
@@ -78,5 +112,68 @@ Page({
     uChartsInstance[e.target.id].scrollEnd(e);
     uChartsInstance[e.target.id].touchLegend(e);
     uChartsInstance[e.target.id].showToolTip(e);
-  }
+    var tapObj = uChartsInstance[e.target.id].getCurrentDataIndex(e);
+    console.log(tapObj)
+    console.log(this.data.userScoreInfo)
+    if (tapObj.index != -1) {
+      this.setData({
+        userScoreValue: this.data.userScoreInfo.scoreValue[tapObj.index],
+        userScoreType: this.data.userScoreInfo.scoreType[tapObj.index],
+        userScoreColor: this.data.userScoreInfo.scoreColor[tapObj.index]
+      })
+      
+    }
+    
+  },
+
+  onLoad(options) {
+    console.log(this.data)
+    console.log("global date: " + app.globalData.userData.mood_track.mood_date)
+    console.log("global score: "  + app.globalData.userData.mood_track.mood_score)
+    console.log(this.getUserScoreDateRange())
+    console.log(this.getUserScoreLevel())
+    this.setData({
+      res_index: 0
+    })
+  },
+
+  chooseOneMonth() {
+    this.setData(
+      {
+        oneMonth: true,
+        threeMonth: false,
+        sixMonth: false,
+        timePeriodText: '近一月',
+        res_index: 0
+      }
+    )
+    this.drawCharts('jkyWEuYZpJWLcfbnKkmySDRjQLEpHsIG', res);
+
+  },
+
+  chooseThreeMonth() {
+    this.setData(
+      {
+        oneMonth: false,
+        threeMonth: true,
+        sixMonth: false,
+        timePeriodText: '近三月',
+        res_index: 1
+      }
+    ),
+    this.drawCharts('jkyWEuYZpJWLcfbnKkmySDRjQLEpHsIG', res);
+  },
+
+  chooseSixMonth() {
+    this.setData(
+      {
+        oneMonth: false,
+        threeMonth: false,
+        sixMonth: true,
+        timePeriodText: '近六月',
+        res_index: 2,
+      }
+    )
+    this.drawCharts('jkyWEuYZpJWLcfbnKkmySDRjQLEpHsIG', res);
+  },
 })
