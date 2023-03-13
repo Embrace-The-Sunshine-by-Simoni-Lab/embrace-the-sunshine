99fcc53ff6265d14dd575f3041e57074b4748bc8
Yawen Guo <jennyguo@uw.edu>
2023-03-13 13:46:59 -0700

    cloud function update

--- cloudfunctions/toggle_podcast_star_status/index.js
+++ cloudfunctions/toggle_podcast_star_status/index.js
@@ -10,16 +10,15 @@ exports.main = async (event, context) => {
   if (wxContext.OPENID != undefined) {
     console.log("user data: " + wxContext.OPENID);
   } else {
-    
     result.errCode = 1;
     result.errMsg = "failed to get user data from WXContext";
     result.data = {};
     return result;
   }
 
-  if (event.podcast_id == undefined) {
+  if (event.podcast_id == undefined || event.mediaType == undefined) {
     result.errCode = 2;
-    result.errMsg = "failed to get podcast_id";
+    result.errMsg = "failed to get podcast_id or media type";
     result.data = {};
     return result;
   }
@@ -39,14 +38,25 @@ exports.main = async (event, context) => {
   if (userData.fav_podcasts == undefined) {
     userData.fav_podcasts = [];
   }
-  
-  if (userData.fav_podcasts[event.podcast_id] == undefined || userData.fav_podcasts[event.podcast_id] === -1) {
-    userData.fav_podcasts[event.podcast_id] = 1;
-  } else if (userData.fav_podcasts[event.podcast_id] === 1) {
-    userData.fav_podcasts[event.podcast_id] = -1;
+
+  if (userData.fav_medi == undefined) {
+    userData.fav_medi = [];
   }
- 
-  
+
+  if (event.mediaType == 'podcast') {
+    if (userData.fav_podcasts[event.podcast_id] == undefined || userData.fav_podcasts[event.podcast_id] === -1) {
+      userData.fav_podcasts[event.podcast_id] = 1;
+    } else if (userData.fav_podcasts[event.podcast_id] === 1) {
+      userData.fav_podcasts[event.podcast_id] = -1;
+    }
+  } else {
+    if (userData.fav_medi[event.podcast_id] == undefined || userData.fav_medi[event.podcast_id] === -1) {
+      userData.fav_medi[event.podcast_id] = 1;
+    } else if (userData.fav_medi[event.podcast_id] === 1) {
+      userData.fav_medi[event.podcast_id] = -1;
+    }
+  }
+
   // update database
   await db.collection('main_db')
   .where({
@@ -54,12 +64,13 @@ exports.main = async (event, context) => {
   })
   .update({
     data: {
-      fav_podcasts: userData.fav_podcasts
+      fav_podcasts: userData.fav_podcasts,
+      fav_medi : userData.fav_medi
     }
   })
 
   return {
-    data: userData.fav_podcasts,
+    data: [userData.fav_podcasts, userData.fav_medi],
     event,
     openid: wxContext.OPENID,
     appid: wxContext.APPID,
