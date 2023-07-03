9e745821e9f15e5df6129dd8a9fe0252db76aeef
Yawen Guo <jennyguo@uw.edu>
2023-07-03 18:04:23 +0800

    resource page complete

--- miniprogram/pages/infoDetail/infoDetail.js
+++ miniprogram/pages/infoDetail/infoDetail.js
@@ -1,30 +1,20 @@
 const app = getApp()
 Page({
   data: {
-    infoTypeList: [
-      {type: "互助小组"}, 
-      {type: "高校"}, 
-      {type: "医院/社会福利机构"}, 
-      {type: "心理咨询机构"}, 
-      {type: "公众号平台"}, 
-      {type: "心理援助热线"},
-      {type: "Apps"}
-    ],
     infoDetail: [],
+    displayNone: "display: none;",
   },
 
   onLoad(options) {
     console.log(options)
-    wx.cloud.callFunction({
-      name: 'getData', // 云函数名称
-      success(res) {
-        const data = res.result; // 获取云函数返回的数据
-        console.log(data);
-        // 处理数据
-      },
-      fail(error) {
-        console.error('调用云函数失败', error);
-      },
+    let all_resources = app.globalData.all_resources
+    all_resources.forEach(element => {
+      if (element.name == options.name) {
+        this.setData({
+          infoDetail: element
+        })
+      }
     });
+    console.log(this.data.infoDetail)
   },
 });
--- miniprogram/pages/infoPage/infoPage.js
+++ miniprogram/pages/infoPage/infoPage.js
@@ -1,7 +1,6 @@
 const app = getApp();
 // pages/infoPage/infoPage.js
 
-
 Page({
   /**
    * Page initial data
@@ -16,69 +15,75 @@ Page({
       {type: "心理咨询机构"}, 
       {type: "公众号平台"}, 
       {type: "心理援助热线"},
-      {type: "Apps"}
     ],
-    infoList: [
-      {
-        "name": "沙丘心理", 
-        "label" : "互助小组",
-        "mediaType": "公众号", 
-        "abstract" : "互助、共情、探索自我", 
-        "fee": "免费",
-        "contact" : "0123456789",
-        "hours" : "10:00 - 17:00",
-        "link" : "公众号主页",
-      }, 
-      {
-        "name": "沙丘心理2", 
-        "label" : "心理咨询机构",
-        "mediaType": "公众号2", 
-        "abstract" : "互助、共情、探索自我2", 
-        "fee": "",
-        "contact" : "0987654321",
-        "hours" : "08:00 - 18:00"
-      }, 
-    ]
+    filterOrNot: false,
+    infoList: [],
+    filteredInfoList: [],
+    filteredInfoTypeList: [],
+    previousType: "",
+    currentType : ""
   },
-
-
 
   jumpToDetailPage(e) {
     let name = e.currentTarget.dataset.id
-    console.log("click to jump to the detail page " + name )
     wx.navigateTo({
       url: '../infoDetail/infoDetail?name=' + name,
     });
   },
 
   scroll(e) {
-    console.log(e)
-  },
-
-  expansionClick(e) {
-    console.log(e)
-    this.setData({
-      isExpand: !this.data.isExpand
-    })
   },
 
   chooseType(e) {
-    console.log("choose type")
+    let infoList = this.data.infoList
+    let infoTypeList = this.data.infoTypeList
+    let filteredInfoList = []
+    let filteredInfoTypeList = infoTypeList
+    let type = e.currentTarget.dataset.id
+    if (this.data.currentType == type) {
+      this.setData({
+        filteredInfoList: infoList, 
+        filteredOrNot: false,
+        currentType: "",
+      })
+    } else {
+      infoList.forEach(element => {
+        if (element.type == type) {
+          filteredInfoList.push(element)
+        }
+      });
+      filteredInfoTypeList.forEach(element => {
+        if (element.type == type) {
+          element.cliked = true
+        }
+      });
+      this.setData({
+        filteredInfoTypeList: filteredInfoTypeList,
+        filteredInfoList: filteredInfoList,
+        filterOrNot: true,
+        currentType: type,
+      })
+    }
   },
 
   /**
    * Lifecycle function--Called when page load
    */
   onLoad: function (options) {
-    let globalData = app.globalData
-    console.log(globalData)
+    let all_resources = app.globalData.all_resources
+    this.setData({
+      infoList: all_resources,
+    })
   },
 
   /**
    * Lifecycle function--Called when page is initially rendered
    */
   onReady: function () {
-
+    let all_resources = app.globalData.all_resources
+    this.setData({
+      infoList: all_resources,
+    })
   },
 
   /**
